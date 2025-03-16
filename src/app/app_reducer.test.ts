ab9e00f0c3813723b25d5f35539b8ec7c53be37a
AlekssKano <aleksskano@gmail.com>
2025-03-16 10:45:51 +1030

    add new version

--- src/app/app_reducer.test.ts
+++ src/app/app_reducer.test.ts
@@ -5,12 +5,12 @@ type InitialState = typeof initialState
 const initialState = {
     themeMode: 'light' as ThemeMode,
 }
-// test('themModeShouldBeAlternative', () => {
-//
-//     const initialState = {
-//         themeMode: 'light' as ThemeMode,
-//     }
-//     const endState= appReducer(initialState, changeModeAC('dark'))
-//
-//     expect(endState.themeMode).toBe('dark')
-// })
+test('themModeShouldBeAlternative', () => {
+
+    const initialState = {
+        themeMode: 'light' as ThemeMode,
+    }
+    // const endState= appReducer(initialState, changeModeAC('dark'))
+    //
+    // expect(endState.themeMode).toBe('dark')
+})
