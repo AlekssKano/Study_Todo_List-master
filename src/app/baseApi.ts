9334b310865cde7b7c26ea00da6adc6bb96345d3
AlekssKano <aleksskano@gmail.com>
2025-04-27 18:25:53 +0930

    newOne

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,19 +1,21 @@
 import { AUTH_TOKEN } from "@/common/constants"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { handleError } from "@/common/utils"
 
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
   tagTypes: ["Todolist", "Task"],
   baseQuery: async (args, api, extraOptions) => {
-    await new Promise((resolve) => setTimeout(resolve, 3000))
-    return fetchBaseQuery(
-      {
-      baseUrl:import.meta.env.VITE_BASE_URL,
-        prepareHeaders: (headers) => {
+    const result = await fetchBaseQuery({
+      baseUrl: import.meta.env.VITE_BASE_URL,
+      prepareHeaders: headers => {
         headers.set("API-KEY", import.meta.env.VITE_API_KEY)
         headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
       }
     })(args, api, extraOptions)
+    handleError(api, result)
+    return result
+
   },
   endpoints: () => ({})
 })
--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -4,9 +4,7 @@ import type { DomainTodolist } from "@/features/todolists/model/todolists-slice"
 import List from "@mui/material/List"
 import { TaskItem } from "./TaskItem/TaskItem"
 import { TasksSkeleton } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksSkeleton/TasksSkeleton.tsx"
-import { setAppErrorAC } from "@/app/app-slice.ts"
-import { useAppDispatch } from "@/common/hooks"
-import { useEffect } from "react"
+
 
 type Props = {
   todolist: DomainTodolist
@@ -15,8 +13,8 @@ type Props = {
 export const Tasks = ({ todolist }: Props) => {
   const { id, filter } = todolist
 
-  const { data, isLoading, error } = useGetTasksQuery(id)
-  const dispatch = useAppDispatch()
+  const { data, isLoading } = useGetTasksQuery(id)
+  // const dispatch = useAppDispatch()
   let filteredTasks = data?.items
   if (filter === "active") {
     filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.New)
@@ -30,17 +28,17 @@ export const Tasks = ({ todolist }: Props) => {
   // if (isError) {
   //   dispatch(setAppErrorAC({ error: error.data.message }))
   // }
-  useEffect(() => {
-    if (!error) return
-    if ('status' in error) {
-      // FetchBaseQueryError
-      const errMsg = 'error' in error ? error.error : JSON.stringify(error.data)
-      dispatch(setAppErrorAC({ error: errMsg }))
-    } else {
-      // SerializedError
-      dispatch(setAppErrorAC({ error: error.message || 'Some error occurred' }))
-    }
-  }, [error])
+
+    // if (error) {
+    //   if ('status' in error) {
+    //     // FetchBaseQueryError
+    //     const errMsg = 'error' in error ? error.error : JSON.stringify(error.data)
+    //     dispatch(setAppErrorAC({ error: errMsg }))
+    //   } else {
+    //     // SerializedError
+    //     dispatch(setAppErrorAC({ error: error.message || 'Some error occurred' }))
+    //   }
+    // }
 
   return (
     <>
